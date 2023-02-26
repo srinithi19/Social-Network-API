d3cf379a241f17d495d8f83b901ac0bbf65e3e0d
Srinithi Ravichandran <emailtonithi@gmail.com>
2023-02-25 20:54:54 -0800

    add updateuser/deleteuser route

--- controllers/userController.js
+++ controllers/userController.js
@@ -33,3 +33,33 @@ const createUser = (req,res) => {
     });
 
 }
+
+const updateUser = (req,res) => {
+    User.findOneAndUpdate(
+        {_id: req.params._id },
+        { $set : req.body },
+        { runValidators : true,
+        new : true })
+        .then((user)=> {
+            if(!user) {
+                res.status(404).json({ message: "no user with this id" });
+            }
+            else {
+                res.json(user)
+            }
+        })
+        .catch((err) => res.status(500).json(err));
+}
+
+const deleteUser = (req,res) => {
+    User.findOneAndDelete({ _id: req.params._id })
+    .then((user) => {
+        !user 
+            ? res.status(404).json({ message: "No user w that id" }) 
+            : Thoughts.deleteMany({
+                _id: { $in: user.thoughts },
+            })
+            res.status(200).json({ message: "Sucessfully deleted" });
+    })
+    .catch((err) => res.status(500).json(err));
+}
