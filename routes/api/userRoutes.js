87dc6aa9975fd24cf1e30dbe47e0c2af344ec717
Srinithi Ravichandran <emailtonithi@gmail.com>
2023-02-25 20:40:30 -0800

    add user routes

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,16 @@
+const router = require("express").Router();
+
+const {
+    getSingleUser,
+    getUsers,
+    createUser,
+    deleteUser,
+    updateUser,
+    deleteFriend,
+    addFriend,
+} = require("../../controllers/userController");
+
+router.route("/").get(getUsers).post(createUser);
+router.route("/:id").get(getSingleUser).delete(deleteUser);
+router.route("/:id/friends/:friendsId").post(addFriend).delete(deleteFriend);
+module.exports = router;
